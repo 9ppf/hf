bee1153d8124f50f05a4d178e6e70607d50a6a3c
9ppf <48794672+9ppf@users.noreply.github.com>
2019-07-18 01:25:14 +0800

    对数据库进行修改，并实现前后端分离，实现放大镜效果

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 //引入模块
 const express=require('express');
 const bodyParser=require('body-parser');
+const cors=require('cors');
 //引入路由器
 const userRouter=require('./routes/user.js');
+const productRouter=require('./routes/product.js');
+// const indexRouter=require('./routes/index.js')
 //创建服务器
 var server=express();
 server.listen(8080);
+// 解决跨域
+server.use(cors({
+  origin:"http://127.0.0.1:5502"
+}))
 //托管静态资源到public目录下
 server.use(express.static('public'));
 //使用body-parser中间件：中间件必须在服务器里使用
@@ -14,3 +21,7 @@ server.use(bodyParser.urlencoded({
 }));
 //挂载路由器，挂载到/user   路由访问  /user/reg
 server.use('/user',userRouter);
+// 挂载路由器，挂载到/product
+server.use('/product',productRouter);
+// 挂载路由，挂载到/index
+// server.use('/index',indexRouter);
